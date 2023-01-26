8fc093ce119c62f2ec58b0c06467cd541362e9dd
checkiamsiam <issiam02415@gmail.com>
2023-01-26 17:09:20 +0600

    besic clear

---
+++ loop.cs
@@ -0,0 +1,30 @@
+using System;
+
+class Root
+{
+    static void Main(string[] args)
+    {
+        // while loop
+        int i = 0;
+        while (i < 5)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+        // for loop
+        for (int j = 0; j < 5; j++)
+        {
+            Console.WriteLine(j);
+        }
+        // do while loop
+        int k = 0;
+        do
+        {
+            Console.WriteLine(k);
+            k++;
+        } while (k < 5);
+
+
+    }
+
+}
